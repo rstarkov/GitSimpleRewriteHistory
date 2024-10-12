72895558bcf036f83a01a4f9ed35d2ff39035ab0
Roman Starkov <sourcecode@roman.st>
2024-10-12 13:44:12 +0100

    Delete obsolete files etc

--- Src/App.xaml.cs
+++ Src/App.xaml.cs
@@ -13,10 +13,5 @@ namespace GitSimpleRewriteHistory
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            System.Windows.Forms.Application.EnableVisualStyles();
-        }
     }
 }
